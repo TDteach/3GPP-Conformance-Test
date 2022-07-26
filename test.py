47767b3153fde62bff8a706d6c1daa09a9a281b9
tdteach <tdteach@gmail.com>
2022-07-26 12:09:41 -0400

    test

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import json
 import numpy as np
+import hashlib
 from sklearn import metrics
 from sentence_transformers import SentenceTransformer, util
-
 
 # model = SentenceTransformer("GPL/quora-msmarco-distilbert-gpl")  #0.899
 # model = SentenceTransformer("GPL/quora-tsdae-msmarco-distilbert-margin-mse")  #0.933
@@ -31,41 +31,67 @@ from sentence_transformers import SentenceTransformer, util
 # model = SentenceTransformer('ChrisZeng/electra-large-discriminator-nli-efl-tweeteval')   #0.652
 
 
-test_file = 'toTD_similarity_testing_data.v1'
+init_file = 'toTD_similarity_testing_data.v1'
 
-def calc_auc(model, return_fps=False):
+def read_init_data():
     line_list = list()
-    with open(test_file,'r') as f:
+    with open(init_file, 'r') as f:
         for line in f:
             line_list.append(line.strip())
 
-    nline = len(line_list)
-    sentences1 = [line_list[i] for i in range(0,nline,2)]
-    sentences2 = [line_list[i] for i in range(1,nline,2)]
-    npairs = len(sentences2)
+    hex_id_map = dict()
+    nsent = 0
+    for line in line_list:
+        hex = hashlib.md5(line.encode()).hexdigest()
+        if not hex in hex_id_map:
+            hex_id_map[hex] = nsent
+            nsent += 1
 
-    embeddings1 = model.encode(sentences1, convert_to_tensor=True)
-    embeddings2 = model.encode(sentences2, convert_to_tensor=True)
+    lab_mat = np.eye(nsent, dtype=np.int32)
+    for i in range(0, len(line_list), 2):
+        s1 = line_list[i]
+        s2 = line_list[i + 1]
+        hex1 = hashlib.md5(s1.encode()).hexdigest()
+        hex2 = hashlib.md5(s2.encode()).hexdigest()
+        id1 = hex_id_map[hex1]
+        id2 = hex_id_map[hex2]
+        lab_mat[id1][id2] = 1
+        lab_mat[id2][id1] = 1
 
-    cosine_scores = util.cos_sim(embeddings1, embeddings2).cpu().numpy()
+    sentences = [None] * nsent
+    for line in line_list:
+        hex = hashlib.md5(line.encode()).hexdigest()
+        id = hex_id_map[hex]
+        sentences[id] = line
 
-    label_mat = compare_fps_data_list()
+    return sentences, hex_id_map, lab_mat
 
-    # for i in range(len(sentences1)):
-    #     print("{} \t\t {} \t\t Score: {:.4f}".format(sentences1[i], sentences2[i], cosine_scores[i][i]))
+
+def embeddings_to_scores(embeddings, lab_mat):
+    n = len(lab_mat)
+    cosine_scores = util.cos_sim(embeddings, embeddings).cpu().numpy()
     scores = list()
     labels = list()
-    for i in range(npairs):
-        for j in range(i+1):
+
+    k_to_pair = dict()
+    k = 0
+    for i in range(n):
+        for j in range(i):
             scores.append(cosine_scores[i][j])
-            labels.append(label_mat[i][j] > 0)
+            labels.append(lab_mat[i][j] > 0)
+            k_to_pair[k] = (i, j)
+            k += 1
     scores, labels = np.asarray(scores), np.asarray(labels)
 
+    return scores, labels, k_to_pair
+
+
+def calc_auc_on_scores(scores, labels, return_fps=False):
     fpr, tpr, thresholds = metrics.roc_curve(labels, scores)
     auc = metrics.auc(fpr, tpr)
     print('auc: ', auc)
 
-    k = np.argmin(fpr+1-tpr)
+    k = np.argmin(fpr + 1 - tpr)
     thr = thresholds[k]
 
     '''
@@ -79,49 +105,34 @@ def calc_auc(model, return_fps=False):
     thr = max_f1_thr
     #'''
 
-
-
-    conf_mat = metrics.confusion_matrix(labels, scores>thr)
+    conf_mat = metrics.confusion_matrix(labels, scores > thr)
     print(conf_mat)
 
-    f1 = metrics.f1_score(labels, scores>thr)
+    f1 = metrics.f1_score(labels, scores > thr)
     print('f1-score: ', f1)
 
-    fps_list = list()
-    cc = 0
-    for i in range(npairs):
-        for j in range(i):
-            if cosine_scores[i][j] > thr and label_mat[i][j] < 0:
-                cc += 1
-                rst = {
-                    'i': i,
-                    'j': j,
-                    'i_sent': sentences1[i],
-                    'j_sent': sentences2[j],
-                    'score': cosine_scores[i][j],
-                }
-                fps_list.append(rst)
-    print(cc, 'new fps samples')
-
     if return_fps:
+        fps_list = list()
+        k = 0
+        for sc, lb in zip(scores, labels):
+            if sc > thr and lb == 0:
+                fps_list.append((k, sc))
+            k += 1
         return auc, fps_list
-
     return auc
 
 
-def read_fps_data(file_path):
-
+def read_fps_data(file_path, n):
     line_list = list()
-    with open(file_path,'r') as f:
+    with open(file_path, 'r') as f:
         for line in f:
             line_list.append(line.strip())
 
+    label_mat = np.eye(n, dtype=np.int32)
 
-    label_mat = np.eye(100, dtype=np.int32)
-
-    for i in range(0,len(line_list),3):
+    for i in range(0, len(line_list), 3):
         line1 = line_list[i]
-        line2 = line_list[i+1]
+        line2 = line_list[i + 1]
         i = int(line1[1:line1.find(']')])
         j = int(line2[1:line2.find(']')])
         label_mat[i][j] = 1
@@ -130,13 +141,13 @@ def read_fps_data(file_path):
     return label_mat
 
 
-def compare_fps_data(predicted_path, labeled_path):
-    pre_mat = read_fps_data(predicted_path)
-    lab_mat = read_fps_data(labeled_path)
+def compare_fps_data(predicted_path, labeled_path, n):
+    pre_mat = read_fps_data(predicted_path, n)
+    lab_mat = read_fps_data(labeled_path, n)
 
-    rst_mat = np.ones([100,100], dtype=np.int32)*-1
-    for i in range(100):
-        for j in range(100):
+    rst_mat = np.ones([n, n], dtype=np.int32) * -1
+    for i in range(n):
+        for j in range(n):
             if pre_mat[i][j] == 1 and lab_mat[i][j] == 1:
                 rst_mat[i][j] = 1
             elif pre_mat[i][j] == 1 and lab_mat[i][j] == 0:
@@ -146,40 +157,69 @@ def compare_fps_data(predicted_path, labeled_path):
 
 
 def merge_lab_mat(ori, new):
-    for i in range(100):
-        for j in range(100):
+    n = len(ori)
+    for i in range(n):
+        for j in range(n):
             if ori[i][j] < 0:
                 ori[i][j] = new[i][j]
             elif new[i][j] < 0:
                 continue
             elif ori[i][j] != new[i][j]:
-                print(i,j,'ori: ', ori[i][j], 'new: ', new[i][j])
+                print(i, j, 'ori: ', ori[i][j], 'new: ', new[i][j])
                 raise NotImplemented
     return ori
 
 
-def compare_fps_data_list():
+def read_data():
+    sents, hex_id_map, lab_mat = read_init_data()
+
     pre_list = list()
     lab_list = list()
-    for i in range(1,17+1):
-        pre_list.append('fps_v%d.txt'%i)
-        lab_list.append('toTD_fps_labeling_v%d.txt'%i)
+    i = 0
+    while True:
+        pre_path = 'fps_v%d.txt' % i
+        lab_path = 'toTD_fps_labeling_v%d.txt' % i
+        if not os.path.exists(lab_path):
+            break
+        pre_list.append(pre_path)
+        lab_list.append(lab_path)
+        i += 1
 
-    lab_mat = np.ones([100,100], dtype=np.int32) * -1
     for pre, lab in zip(pre_list, lab_list):
-        rst_mat = compare_fps_data(pre, lab)
+        rst_mat = compare_fps_data(pre, lab, len(sents))
         lab_mat = merge_lab_mat(lab_mat, rst_mat)
 
-    return lab_mat
+    return sents, hex_id_map, lab_mat
 
 
+def evaluate_model(model, return_fps=False):
+    sents, hex_id_map, lab_mat = read_data()
 
+    embeddings = model.encode(sents, convert_to_tensor=True)
+    scores, labels, k_to_pair = embeddings_to_scores(embeddings, lab_mat)
+
+    if not return_fps:
+        return calc_auc_on_scores(scores, labels, return_fps=False)
+    else:
+        auc, fps_list = calc_auc_on_scores(scores, labels, return_fps=True)
+        ret_list = list()
+        for k, sc in fps_list:
+            i, j = k_to_pair[k]
+            _dict = {
+                'i' = i,
+                'j' = j,
+                'i_sent' = sents[i],
+                'j_sent' = sents[j],
+                'score' = sc,
+            }
+            ret_list.append(_dict)
+        return auc, ret_list
 
 
 if __name__ == '__main__':
     model_path_list = list()
     model_path_list.append('stsb-distilbert-base')
-    for i in range(1,14+1):
+    for i in range(1, 14 + 1):
         model_path = 'output/3GPP/{}0000'.format(i)
         model_path_list.append(model_path)
     model_path_list.append("GPL/quora-tsdae-msmarco-distilbert-margin-mse")
@@ -193,18 +233,17 @@ if __name__ == '__main__':
     for i, model_path in enumerate(model_path_list):
         print(model_path)
         model = SentenceTransformer(model_path)
-        auc = calc_auc(model)
-        rst_list.append((i,auc))
-        print('[',i,']-------------------------------------')
+        auc = evaluate_model(model)
+        rst_list.append((i, auc))
+        print('[', i, ']-------------------------------------')
 
     model = SentenceTransformer(model_path_list[-2])
-    auc, fps_list = calc_auc(model, return_fps=True)
-    with open('fps.txt','w') as f:
+    auc, fps_list = evaluate_model(model, return_fps=True)
+    with open('fps.txt', 'w') as f:
         for fps in fps_list:
-            f.write('['+str(fps['i'])+']'+' '+fps['i_sent']+'\n')
-            f.write('['+str(fps['j'])+']'+' '+fps['j_sent']+'\n')
-            f.write(str(fps['score'])+'\n')
-
+            f.write('[' + str(fps['i']) + ']' + ' ' + fps['i_sent'] + '\n')
+            f.write('[' + str(fps['j']) + ']' + ' ' + fps['j_sent'] + '\n')
+            f.write(str(fps['score']) + '\n')
 
 '''
 with open('generated/3GPP/corpus.jsonl', 'r') as json_file:
@@ -229,5 +268,4 @@ print(bin_edges)
 with open('haha.jsonl','w') as f:
     for json_str in rst_str:
         f.write(json_str)
-'''
-
+#'''
